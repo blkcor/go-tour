303f303cc70ec0ef2554bd4f90597748492b8581
blkcor <1029129867@qq.com>
2023-06-21 16:59:08 +0800

    feat: config management

---
+++ blog-service/pkg/setting/section.go
@@ -0,0 +1,46 @@
+package setting
+
+import "time"
+
+type ServerSettingS struct {
+	RunMode      string
+	HttpPort     string
+	ReadTimeout  time.Duration
+	WriteTimeout time.Duration
+}
+
+type AppSettingS struct {
+	DefaultPageSize int
+	MaxPageSize     int
+	LogSavePath     string
+	LogFileName     string
+	LogFileExt      string
+}
+
+type DatabaseSettingS struct {
+	DBType       string
+	UserName     string
+	Password     string
+	Host         string
+	DBName       string
+	TablePrefix  string
+	Charset      string
+	ParseTime    bool
+	MaxIdleConns int
+	MaxOpenConns int
+}
+
+/*
+call like this:
+
+	var serverSettings ServerSettingS
+	s.ReadSection("server", &serverSettings)
+	它会从配置文件读取 "server" section, 并解析到 serverSettings 结构体中。
+*/
+func (s *Setting) ReadSection(k string, v interface{}) error {
+	err := s.vp.UnmarshalKey(k, v)
+	if err != nil {
+		return err
+	}
+	return nil
+}
