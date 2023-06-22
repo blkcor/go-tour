863aaa07995feec58c0794d2f8785cb4ecbb8fbf
blkcor <1029129867@qq.com>
2023-06-22 16:45:32 +0800

    feat: swagger doc configuration

--- blog-service/internal/routers/api/v1/article.go
+++ blog-service/internal/routers/api/v1/article.go
@@ -10,10 +10,56 @@ func NewArticle() Article {
 	return Article{}
 }
 
-func (a Article) Get(c *gin.Context) {
+// @Summary 获取单篇文章
+// @Produce  json
+// @Param name query string false "文章名称" maxlength(100)
+// @Param state query int false "状态" Enums(0, 1) default(1)
+// @Success 200 {object} model.Tag "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles/{id} [get]func (a Article) Get(c *gin.Context)   {}
+func (a Article) Get(c *gin.Context) {}
 
-}
-func (a Article) List(c *gin.Context)   {}
+// @Summary 获取多篇文章
+// @Produce  json
+// @Param name query string false "文章名称" maxlength(100)
+// @Param state query int false "状态" Enums(0, 1) default(1)
+// @Param page query int false "页码"
+// @Param page_size query int false "每页数量"
+// @Success 200 {object} model.Article "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles [get]func (a Article) List(c *gin.Context)   {}
+func (a Article) List(c *gin.Context) {}
+
+// @Summary 新增文章
+// @Produce  json
+// @Param name body string true "文章名称" minlength(3) maxlength(100)
+// @Param state body int false "状态" Enums(0, 1) default(1)
+// @Param created_by body string true "创建者" minlength(3) maxlength(100)
+// @Success 200 {object} model.Article "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles [post]
 func (a Article) Create(c *gin.Context) {}
+
+// @Summary 更新文章
+// @Produce  json
+// @Param id path int true "文章 ID"
+// @Param name body string false "文章名称" minlength(3) maxlength(100)
+// @Param state body int false "状态" Enums(0, 1) default(1)
+// @Param modified_by body string true "修改者" minlength(3) maxlength(100)
+// @Success 200 {array} model.Article "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles/{id} [put]
 func (a Article) Update(c *gin.Context) {}
+
+// @Summary 删除文章
+// @Produce  json
+// @Param id path int true "文章 ID"
+// @Success 200 {string} string "成功"
+// @Failure 400 {object} errcode.Error "请求错误"
+// @Failure 500 {object} errcode.Error "内部错误"
+// @Router /api/v1/articles/{id} [delete]
 func (a Article) Delete(c *gin.Context) {}
--- blog-service/main.go
+++ blog-service/main.go
@@ -15,6 +15,10 @@ import (
 	"time"
 )
 
+// @title 博客系统
+// @version 1.0
+// @description Go 语言编程之旅：一起用 Go 做项目
+// @termsOfService https://github.com/go-programming-tour-book
 func init() {
 	//setting
 	err := setupSetting()
