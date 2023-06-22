8be9028daebeda5cb756fff3f311825a33bd50ee
blkcor <1029129867@qq.com>
2023-06-22 17:42:34 +0800

    chore: change position

--- blog-service/internal/model/article.go
+++ blog-service/internal/model/article.go
@@ -16,33 +16,6 @@ type ArticleSwagger struct {
 	Pager *app.Pager
 }
 
-type CountArticleRequest struct {
-	Title string `form:"title" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
-}
-
-type ArticleListRequest struct {
-	Title string `form:"title" binding:"max=100"`
-	State uint8  `form:"state,default=1" binding:"oneof=0 1"`
-}
-
-type CreateArticleRequest struct {
-	Title     string `form:"title" binding:"required,min=3,max=100"`
-	CreatedBy string `form:"created_by" binding:"required,min=3,max=100"`
-	State     uint8  `form:"state,default=1" binding:"oneof=0 1"`
-}
-
-type UpdateArticleRequest struct {
-	ID         uint32 `form:"id" binding:"required,gte=1"`
-	Title      string `title:"name" binding:"min=3,max=100"`
-	State      uint8  `form:"state" binding:"required,oneof=0 1"`
-	ModifiedBy string `form:"modified_by" binding:"required,min=3,max=100"`
-}
-
-type DeleteArticleRequest struct {
-	ID uint32 `form:"id" binding:"required,gte=1"`
-}
-
 func (a Article) TableName() string {
 	return "blog_article"
 }
