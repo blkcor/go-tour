32142d0385894f6755ba03f24c66be67a1eb7c4b
blkcor <1029129867@qq.com>
2023-07-10 15:00:29 +0800

    chore: provide http service port

--- tag-sercice/main.go
+++ tag-sercice/main.go
@@ -7,25 +7,61 @@ import (
 	"github.com/go-programming-tour-book/tag-service/server"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
+	"log"
 	"net"
+	"net/http"
 )
 
-var port string
+var http_port string
+var grpc_port string
 
 func init() {
-	flag.StringVar(&port, "port", "8001", "启动端口号")
+	flag.StringVar(&grpc_port, "grpc_port", "8001", "grpc服务启动端口号")
+	flag.StringVar(&http_port, "http_port", "9001", "http服务启动端口号")
 	flag.Parse()
 }
 func main() {
+	errs := make(chan error)
+
+	//rpc service
+	go func() {
+		err := RunRpcServer(grpc_port)
+		if err != nil {
+			errs <- err
+			fmt.Printf("rpc service start err: %v", err)
+		}
+	}()
+
+	//http service
+	go func() {
+		err := RunHttpServer(http_port)
+		if err != nil {
+			errs <- err
+			fmt.Printf("http service start err: %v", err)
+		}
+	}()
+	select {
+	case err := <-errs:
+		log.Fatalf("Run server err: %v", err)
+	}
+}
+
+func RunHttpServer(port string) error {
+	serverMux := http.NewServeMux()
+	serverMux.HandleFunc("/ping", func(writer http.ResponseWriter, request *http.Request) {
+		_, _ = writer.Write([]byte("pong"))
+	})
+	return http.ListenAndServe(":"+port, serverMux)
+}
+
+func RunRpcServer(port string) error {
 	s := grpc.NewServer()
 	pb.RegisterTagServiceServer(s, server.NewTagServer())
 	reflection.Register(s)
+	//grpc service
 	lis, err := net.Listen("tcp", ":"+port)
 	if err != nil {
 		fmt.Printf("net.Listen err: %v", err)
 	}
-	err = s.Serve(lis)
-	if err != nil {
-		fmt.Printf("serve.Serve err: %v", err)
-	}
+	return s.Serve(lis)
 }
